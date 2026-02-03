11660d4d507ec24f3e737753f4d21c652ff4dd58
tBug <galahador@protonmail.com>
2026-02-03 19:11:24 +0100

    New SecurityStatus

---
+++ Sources/DeviceSecurityKit/Models/SecurityStatus.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public enum SecurityStatus: Equatable {
+    case secure
+    case jailbroken
+    case debuggerAttached
+    case emulator
+    case reverseEngineered
+    case compromised
+    
+    public var isSecure: Bool {
+        return self == .secure
+    }
+    
+    public var description: String {
+        switch self {
+        case .secure:
+            return "Device is secure"
+        case .jailbroken:
+            return "Device is jailbroken"
+        case .debuggerAttached:
+            return "Debugger is attached"
+        case .emulator:
+            return "Running in emulator"
+        case .reverseEngineered:
+            return "App has been tampered"
+        case .compromised:
+            return "Device is compromised"
+        }
+    }
+}
